5b39c45b591fa98e90d3332939212278ebe6db80
Daniel Naval <danielnaval2702@gmail.com>
2021-09-10 17:31:09 -0400

    Update login.php
    Use the login sample from Bootstrap examples

--- views/login.php
+++ views/login.php
@@ -6,8 +6,63 @@ $fx = new dnfunctions();
 
 ?>
 
+<style>
+.bd-placeholder-img {
+font-size: 1.125rem;
+text-anchor: middle;
+-webkit-user-select: none;
+-moz-user-select: none;
+user-select: none;
+}
 
-<div class="container">
+@media (min-width: 768px) {
+.bd-placeholder-img-lg {
+    font-size: 3.5rem;
+}
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
+
+<main class="form-signin text-center">
     <?php
         if(isset($_SESSION['lalert'])) {
             echo $_SESSION['lalert'];
@@ -15,25 +70,27 @@ $fx = new dnfunctions();
         }
     ?>
     <?php if((empty($_GET['chp']))||($_GET['chp']!=1)) {?>
-        <h1>Login to UserAdmin</h1>
+        <!-- <h1>Login to UserAdmin</h1> -->
     
         <div class="login-form">
             <form method="post" action="./actions/conAction.php">
-                <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-4">
-                    <input type="email" class="form-control" id="inputEmail3" name="log">
-                    </div>
+
+                <img class="mb-4" src="./images/Naval_Daniel_Logo.png" alt="" width="90" height="90">
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="log">
+                <label for="floatingInput">Email address</label>
                 </div>
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-4">
-                    <input type="password" class="form-control" id="inputPassword3" name="mdp">
-                    </div>
+
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="mdp">
+                <label for="floatingPassword">Password</label>
                 </div>
+
                 <input type="hidden" name="csrf_token" value="<?php echo $fx->gen_token();?>" />
-                
-                <button type="submit" class="btn btn-primary">LOG-IN</button>
+               
+                <button class="w-100 btn btn-lg btn-primary" type="submit">LOG-IN</button>
 
             </form>
         </div>
@@ -45,25 +102,24 @@ $fx = new dnfunctions();
         <div class="login-form">
             <form method="post" action="./actions/conAction.php">
 
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">New Password</label>
-                    <div class="col-sm-4">
-                    <input type="password" class="form-control" id="inputPassword3" name="pass1">
-                    </div>
+                <img class="mb-4" src="./images/Naval_Daniel_Logo.png" alt="" width="90" height="90">
+                <h1 class="h3 mb-3 fw-normal">Please reset you password</h1>
+
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com" name="pass1">
+                <label for="floatingInput">Email address</label>
                 </div>
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Confirm Password</label>
-                    <div class="col-sm-4">
-                    <input type="password" class="form-control" id="inputPassword3" name="pass2">
-                    </div>
+
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="pass2">
+                <label for="floatingPassword">Password</label>
                 </div>
+
                 <input type="hidden" name="csrf_token" value="<?php echo $fx->gen_token();?>" />
-                
-                <button type="submit" class="btn btn-primary">RESET</button>
+               
+                <button class="w-100 btn btn-lg btn-primary" type="submit">RESET</button>
 
             </form>
         </div>
     <?php } ?>    
-</div>
-
-
+</main>
